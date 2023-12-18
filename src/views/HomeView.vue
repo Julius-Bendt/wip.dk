93f10b67bb6e9598942ae923fbedc21e6d4d06cc
JUB\juliu <julius@juto.dk>
2023-12-18 22:02:49 +0100

    Scaffolding and initial welcome page

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <main class="flex h-screen w-screen justify-center">
+    <content class="text-center my-auto">
+      <h1 className="text-8xl font-bold">Hello world!</h1>
+      <h2 class="text-4xl">We're working hard on getting this site ready!</h2>
+      <p>Once ready, this site will hold some toolings that I personally use.</p>
+      <h3 class="mt-6 text-2xl">My plans for this site includes:</h3>
+      <ul>
+        <li>1. A site for copy&paste my favorite color scheme (which I use regularly)</li>
+        <li>2. A feature which uses PGP and AES for en- and decrypting data</li>
+        <li>3. Listing different programs I use in my day to day life</li>
+        <li>4. Listing projects, that I don't necessary think fits my portfolio</li>
+      </ul>
+      <p class="mt-4">
+        In the meanwhile, check out my portfolio at
+        <a class="text-purple-600" href="https://jub.dk" target="_blank">jub.dk</a>
+      </p>
+      <p>
+        Do you have any questions, comments or just wanna talk tech? send me a mail at
+        <a class="text-purple-600" href="mailto:secret@wip.dk">secret@wip.dk</a>
+      </p>
+    </content>
+  </main>
+</template>
